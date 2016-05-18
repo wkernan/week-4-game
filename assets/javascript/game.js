5a70f457b4336c63041c351b633fca36f3fa712f
wkernan@gmail.com <wkernan@gmail.com>
2016-05-17 22:06:12 -0500

    changed back to bootcamp

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,39 +11,42 @@ $(document).ready(function() {
 	var players = [
 		{
 			id: "first",
-			name: "Steve",
+			name: "Bill Kernan",
 			health: 100,
 			atkPower: 10,
 			ctrAtkPower: 20,
-			pic: '../assets/images/steve.png'
-			/*pic: 'https://secure.gravatar.com/avatar/ee340231d2d8c8c380070273506065c3.jpg?s=512d=https%3A%2F%2Fa.slack-edge.com%2F66f9%2Fimg%2Favatars%2Fava_0008-72.png'*/
+			/*pic: '../assets/images/steve.png'*/
+			pic: 'https://secure.gravatar.com/avatar/ee340231d2d8c8c380070273506065c3.jpg?s=512d=https%3A%2F%2Fa.slack-edge.com%2F66f9%2Fimg%2Favatars%2Fava_0008-72.png'
 		},
 
 		{
 			id: "second",
-			name: "Jamie MacWilliams",
+			name: "Phillip Cook",
 			health: 130,
 			atkPower: 10,
 			ctrAtkPower: 8,
-			pic: '../assets/images/jamie.jpg'	
+			pic: 'https://avatars.slack-edge.com/2016-04-27/38179150022_5b0a09f62f0897206ccd_512.png'
+			/*pic: '../assets/images/jamie.jpg'*/	
 		},
 
 		{
 			id: "third",
-			name: "Creeper",
+			name: "Josh Madewell",
 			health: 160,
 			atkPower: 6,
 			ctrAtkPower: 8,
-			pic: '../assets/images/creeper.jpg'	
+			/*pic: '../assets/images/creeper.jpg'*/
+			pic: 'https://secure.gravatar.com/avatar/4090f8251ba508c6db1a1bd5c8ec20c3.jpg?s=512&d=https%3A%2F%2Fa.slack-edge.com%2F7fa9%2Fimg%2Favatars%2Fava_0021-512.png'	
 		},
 
 		{
 			id: "fourth",
-			name: "Enderman",
+			name: "Roger Le",
 			health: 190,
 			atkPower: 4,
 			ctrAtkPower: 12,
-			pic: '../assets/images/enderman.png'	
+			/*pic: '../assets/images/enderman.png'*/
+			pic: 'https://secure.gravatar.com/avatar/5edbb37e96d66ce7510435a8291701a1.jpg?s=512&d=https%3A%2F%2Fa.slack-edge.com%2F7fa9%2Fimg%2Favatars%2Fava_0025-512.png'	
 		},
 
 	];
@@ -54,6 +57,7 @@ $(document).ready(function() {
 			var hlth = $('<div>').addClass('progress-bar progress-bar-success').attr({"role":'progressbar', "aria-valuenow": '40', "aria-valuemin": '0', "aria-valuemax": players[i].health, "style": 'width: 100%'}).text(players[i].health);
 			var divHlth = $('<div>').addClass('progress').append(hlth);
 			var p = $('<p>').text(players[i].name);
+			p.addClass('name');
 			var rowDiv = $('<div>');
 			rowDiv.addClass('inline pick text-center col-md-3 col-xs-6').append(img).append(divHlth).append(p);
 			$('#choosePlayer').append(rowDiv);
@@ -101,6 +105,8 @@ $(document).ready(function() {
 			$('.defender').find('.progress-bar').text(defHlth);
 			$('#struck').text('You were counterattacked by ' + defName + ' for ' + defCtrAtk);
 			$('#hit').text('You attack ' + defName + ' for ' + heroAtk);
+			$('.player img').effect('bounce');
+			$('.defender img').effect('bounce');
 			heroAtk += parseInt(playAtk);
 			if(playHlth/playMaxHlth*100 < 50) {
 				$('.player').find('.progress-bar').removeClass('progress-bar-success').addClass('progress-bar-warning');
@@ -126,9 +132,6 @@ $(document).ready(function() {
 				$('.reset').removeClass('hide');
 			}
 		}
-		console.log(heroAtk);
-		console.log(playHlth);
-		console.log(defHlth);
 		if($.trim($("#enemies").html())=='' && $.trim($("#defender").html()) =='') {
 			alert('You Win');
 			$('.reset').removeClass('hide');
